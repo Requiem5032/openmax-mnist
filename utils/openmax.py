22c4ac02e8b609f5bf210268d8f459f89486e163
Requiem <public_receiver035@pm.me>
2024-03-29 11:33:00 +0700

    initial commit

---
+++ utils/openmax.py
@@ -0,0 +1,117 @@
+import numpy as np
+import pickle
+
+from utils.evt_fitting import query_weibull
+from utils.evt_fitting import weibull_tailfitting
+from utils.openmax_utils import *
+
+IMG_DIM = 28
+NCLASSES = 10
+ALPHA_RANK = 1
+WEIBULL_TAIL_SIZE = 10
+MODEL_PATH = 'models/weibull_model.pkl'
+
+labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def create_model(model, data):
+    x_train, x_test, y_train, y_test = data
+    x_all = np.concatenate((x_train, x_test), axis=0)
+    y_all = np.concatenate((y_train, y_test), axis=0)
+
+    logits_outputs, softmax_outputs = get_activations(x_all, model)
+    correct_index = get_correct_classified(softmax_outputs, y_all)
+
+    logits_correct = logits_outputs[correct_index]
+    y_correct = y_all[correct_index]
+
+    av_map = {}
+
+    for label in labels:
+        av_map[label] = logits_correct[(y_correct == label), :]
+
+    feature_mean = []
+    feature_distance = []
+
+    for label in labels:
+        mean = compute_mean_vector(av_map[label])
+        distance = compute_distance_dict(mean, av_map[label])
+        feature_mean.append(mean)
+        feature_distance.append(distance)
+
+    build_weibull(mean=feature_mean, distance=feature_distance,
+                  tail=WEIBULL_TAIL_SIZE)
+
+
+def build_weibull(mean, distance, tail):
+    weibull_model = {}
+
+    for label in labels:
+        weibull_model[label] = {}
+        weibull = weibull_tailfitting(
+            mean[label], distance[label], tailsize=tail)
+        weibull_model[label] = weibull
+
+    with open(MODEL_PATH, 'wb') as file:
+        pickle.dump(weibull_model, file)
+
+
+def recalibrate_scores(weibull_model, labels, activation_vector, alpharank=ALPHA_RANK, distance_type='eucos'):
+    ranked_list = activation_vector.argsort().ravel()[::-1]
+    alpha_weights = [
+        ((alpharank+1) - i) / float(alpharank) for i in range(1, alpharank+1)
+    ]
+    ranked_alpha = np.zeros(NCLASSES)
+
+    for i in range(len(alpha_weights)):
+        ranked_alpha[ranked_list[i]] = alpha_weights[i]
+
+    # print(ranked_alpha)
+
+    openmax_scores = []
+    openmax_scores_u = []
+
+    for label in labels:
+        weibull = query_weibull(label, weibull_model, distance_type)
+        av_distance = compute_distance(
+            weibull[1], activation_vector.ravel())
+        wscore = weibull[2][0].w_score(av_distance)
+        # print(f'wscore_{label}: {wscore}')
+        modified_score = activation_vector[0][label] * \
+            (1 - wscore*ranked_alpha[label])
+        openmax_scores += [modified_score]
+        openmax_scores_u += [activation_vector[0][label] - modified_score]
+
+    openmax_scores = np.array(openmax_scores)
+    openmax_scores_u = np.array(openmax_scores_u)
+
+    # print(f'activation_vector: {activation_vector}')
+    # print(f'openmax_scores: {openmax_scores}')
+    # print(f'openmax_scores_u: {np.sum(openmax_scores_u)}')
+
+    openmax_probab, prob_u = computeOpenMaxProbability(
+        openmax_scores, openmax_scores_u, labels)
+    return np.array(openmax_probab), np.array(prob_u)
+
+
+def computeOpenMaxProbability(openmax_scores, openmax_scores_u, labels):
+    exp_scores = []
+
+    for label in labels:
+        exp_scores += [np.exp(openmax_scores[label])]
+
+    total_denominator = np.sum(
+        np.exp(openmax_scores) + np.exp(np.sum(openmax_scores_u)))
+    prob_k = np.array(exp_scores) / total_denominator
+    prob_u = np.exp(np.sum(openmax_scores_u)) / total_denominator
+
+    res = prob_k.tolist() + [prob_u]
+    return res, prob_u
+
+
+def compute_openmax(activation_vector):
+    with open(MODEL_PATH, 'rb') as file:
+        weibull_model = pickle.load(file)
+    openmax, prob_u = recalibrate_scores(
+        weibull_model, labels, activation_vector)
+    return openmax, prob_u
---
+++ utils/openmax_utils.py
@@ -0,0 +1,157 @@
+import tensorflow as tf
+import scipy.spatial.distance as spd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
+from tensorflow.keras.datasets import mnist, fashion_mnist
+
+IMG_DIM = 28
+
+
+def compute_mean_vector(feature):
+    return np.mean(feature, axis=0)
+
+
+def compute_distance(mean_feature, feature, distance_type='eucos'):
+    if distance_type == 'eucos':
+        query_distance = spd.euclidean(
+            mean_feature, feature)/200. + spd.cosine(mean_feature, feature)
+    elif distance_type == 'euclidean':
+        query_distance = spd.euclidean(mean_feature, feature)/200.
+    elif distance_type == 'cosine':
+        query_distance = spd.cosine(mean_feature, feature)
+    else:
+        print('Distance type unknown, valid distance type: eucos, euclidean, cosine')
+    return query_distance
+
+
+def compute_distance_dict(mean_feature, feature):
+    eu_dist, cos_dist, eucos_dist = [], [], []
+    for feat in feature:
+        eu_dist += [spd.euclidean(mean_feature, feat)/200.]
+        cos_dist += [spd.cosine(mean_feature, feat)]
+        eucos_dist += [spd.euclidean(mean_feature, feat)/200. + spd.cosine(
+            mean_feature, feat)]
+    distances = {'eucos': eucos_dist, 'cosine': cos_dist, 'euclidean': eu_dist}
+    return distances
+
+
+def get_train_test():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    x_train, x_test = x_train / 255., x_test / 255.
+
+    x_train = x_train.reshape(-1, IMG_DIM, IMG_DIM, 1)
+    x_test = x_test.reshape(-1, IMG_DIM, IMG_DIM, 1)
+    return x_train, x_test, y_train, y_test
+
+
+def get_train_test_fashion():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    x_train, x_test = x_train / 255., x_test / 255.
+
+    x_train = x_train.reshape(-1, IMG_DIM, IMG_DIM, 1)
+    x_test = x_test.reshape(-1, IMG_DIM, IMG_DIM, 1)
+    return x_train, x_test, y_train, y_test
+
+
+def get_eval_data(include_train=False):
+    x_known_train, x_known_test, y_known_train, y_known_test = get_train_test()
+    x_unknown_train, x_unknown_test, y_unknown_train, y_unknown_test = get_train_test_fashion()
+
+    y_unknown_train = np.full(y_unknown_train.shape, 10)
+    y_unknown_test = np.full(y_unknown_test.shape, 10)
+
+    if include_train:
+        x_all = np.concatenate(
+            (x_known_train, x_known_test, x_unknown_train, x_unknown_test), axis=0)
+        y_all = np.concatenate(
+            (y_known_train, y_known_test, y_unknown_train, y_unknown_test), axis=0)
+    else:
+        x_all = np.concatenate((x_known_test, x_unknown_test), axis=0)
+        y_all = np.concatenate((y_known_test, y_unknown_test), axis=0)
+    return x_all, y_all
+
+
+def get_openmax_predict(openmax, threshold=0.0001):
+    if openmax[10] >= threshold:
+        res = 10
+    else:
+        temp = np.delete(openmax, -1)
+        res = np.argmax(temp)
+    return res
+
+
+def convert_label(y):
+    y[y != 10] = 0
+    y[y == 10] = 1
+    return y
+
+
+def get_activations(x, model):
+    partial_model = tf.keras.models.Model(
+        inputs=model.input,
+        outputs=[
+            model.get_layer('logits').output,
+            model.get_layer('softmax').output,
+        ],
+    )
+    logits_outputs, softmax_outputs = partial_model.predict(x)
+    return logits_outputs, softmax_outputs
+
+
+def get_correct_classified(prob, y):
+    pred_y = np.argmax(prob, axis=1)
+    res = pred_y == y
+    return res
+
+
+def compute_roc(y_true, y_score):
+    fpr, tpr, thresholds = roc_curve(y_true, y_score)
+    auroc = roc_auc_score(y_true, y_score)
+    roc = {
+        'fpr': fpr,
+        'tpr': tpr,
+        'thresholds': thresholds,
+        'auroc': auroc,
+    }
+    return roc
+
+
+def compute_pr(y_true, y_score):
+    precision, recall, thresholds = precision_recall_curve(y_true, y_score)
+    aupr = average_precision_score(y_true, y_score)
+    pr = {
+        'precision': precision,
+        'recall': recall,
+        'thresholds': thresholds,
+        'aupr': aupr,
+    }
+    return pr
+
+
+def plot_roc(roc):
+    plt.plot(roc['fpr'], roc['tpr'])
+    plt.grid('on')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('AUROC: {:.5f}'.format(roc['auroc']))
+
+
+def plot_pr(pr):
+    plt.plot(pr['recall'], pr['precision'])
+    plt.grid('on')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('AUPR: {:.5f}'.format(pr['aupr']))
+
+
+def image_show(img):
+    plt.imshow(np.squeeze(img), cmap='gray')
+    plt.show()
